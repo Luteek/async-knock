ca25cbcf2ad16fbe5e0a3ad4d854bac2f70a63f0
luteek <develop.luteek@gmail.com>
2017-10-02 13:00:51 +0300

    change params

--- main.py
+++ main.py
@@ -6,24 +6,21 @@ CONFIG_FILE = "config.ini"
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
 
-try:
-    prom = config['PROMETHEUS']
-    host = prom['host']
-    jobName = prom['jobName']
+prom = config['PROMETHEUS']
+host = prom['host']
+jobName = prom['jobName']
 
-    thread = config['THREAD']
-    max_thread = thread['max_thread']
-    delay_ping = thread['delay_ping']
-    delay_parse = thread['delay_parse']
+thread = config['THREAD']
+max_thread = thread['max_thread']
+delay_ping = thread['delay_ping']
+delay_parse = thread['delay_parse']
 
-    pngparam = config['PING_PARAM']
-    png_norm = pngparam['norma']
-    png_large = pngparam['large']
-    png_fast = pngparam['fast']
-    png_killing = pngparam['killing']
+pngparam = config['PING_PARAM']
+png_norm = pngparam['normal']
+png_large = pngparam['large']
+png_fast = pngparam['fast']
+png_killing = pngparam['killing']
 
-    work = worker.Worker(host, jobName, max_thread, [png_norm, png_large, png_fast, png_killing], 30, 60)
-    work.loop.run_until_complete(work.start_config())
-except:
-    print('Error read configuration file')
+work = worker.Worker(host, jobName, max_thread, [png_norm, png_large, png_fast, png_killing], 30, 60)
+work.loop.run_until_complete(work.start_config())
 
--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ from subprocess import Popen, PIPE
 import re
 import logging
 
-logging.basicConfig(filename='log.log', level=logging.DEBUG, format='%(asctime)s -%(levelname)s -%(message)s')
+#logging.basicConfig(filename='log.log', level=logging.DEBUG, format='%(asctime)s -%(levelname)s -%(message)s')
 
 
 class Worker:
@@ -20,16 +20,17 @@ class Worker:
     metric_delay_min = Gauge('ping_pkt_latency_min', 'l', ['ip', 'group'], registry=registry)
     metric_delay_max = Gauge('ping_pkt_latency_max', 'l', ['ip', 'group'], registry=registry)
 
-    def __init__(self, gateway_host, job, max_thread, png_param, delay_ping=30, delay_parse=60):
+    def __init__(self, gateway_host, job, max_thread, png_param, delay_ping, delay_parse):
         self.gateway_host = gateway_host
         self.job = job
-        self.MAX_THREAD_COUNT = max_thread
+        self.MAX_THREAD_COUNT = int(max_thread)
         self.png_normal = png_param[0]
         self.png_large = png_param[1]
         self.png_fast = png_param[2]
         self.png_killing = png_param[3]
-        self.delay_ping = delay_ping[4]
-        self.delay_parse = delay_parse[5]
+        self.delay_ping = int(delay_ping)
+        self.delay_parse = int(delay_parse)
+        print('Delay ping %s' %self.delay_ping)
 
     def load_config(self):
         self.task_remove = [' ']
@@ -54,16 +55,16 @@ class Worker:
             if must_be_add:
                 self.task_add.append(new_task.split(';'))
                 self.host_config.append(new_task.split(';'))
-        logging.info(u'LOAD_CONFIG:%s' % self.host_config)
+        print('LOAD_CONFIG:%s' % self.host_config)
+        #logging.info(u'LOAD_CONFIG:%s' % self.host_config)
 
     @asyncio.coroutine
     def start_config(self):
-        # self.loop.create_task(self.request_metrics())
         while True:
             self.load_config()
             for config in self.task_add:
                 self.loop.create_task(self.ping_host(config[0], config[1], config[2]))
-            yield from asyncio.sleep(60)
+            yield from asyncio.sleep(self.delay_parse)
 
     # @asyncio.coroutine
     # def request_metrics(self):
@@ -97,48 +98,63 @@ class Worker:
                 """TEST"""
                 if self.THREAD_COUNT < self.MAX_THREAD_COUNT:
                     self.THREAD_COUNT = self.THREAD_COUNT + 1
-                    logging.info(u'start ping ip %s' % ip)
+
+                    print('start ping ip %s' % ip)
+                    #logging.info(u'start ping ip %s' % ip)
                     line = ('ping' + ' ' + ip + ' ' + parameters)
                     cmd = Popen(line.split(' '), stdout=PIPE)
                     yield from asyncio.sleep(0)
+
                     output = cmd.communicate()[0]
                     self.THREAD_COUNT = self.THREAD_COUNT - 1
 
-                    # find loss
-                    result_loss = re.split(r'% packet loss', str(output))
-                    result_loss = re.split(r'received, ', result_loss[0])
-                    # find avr
-                    result = re.split(r'mdev = ', str(output))
-                    result = re.split(r' ms', result[1])
-                    result = re.split(r'/', result[0])
-                    result_loss[0] = result_loss[1]
-                    # rtt avg
-                    result_loss[1] = result[1]
-                    # rtt min
-                    result_loss.append(result[0])
-                    # rtt max
-                    result_loss.append(result[2])
-                    data = result_loss
-
+                    try:
+                        # find loss
+                        result_loss = re.split(r'% packet loss', str(output))
+                        result_loss = re.split(r'received, ', result_loss[0])
+                        # find avr
+                        result = re.split(r'mdev = ', str(output))
+                        result = re.split(r' ms', result[1])
+                        result = re.split(r'/', result[0])
+                        result_loss[0] = result_loss[1]
+                        # rtt avg
+                        result_loss[1] = result[1]
+                        # rtt min
+                        result_loss.append(result[0])
+                        # rtt max
+                        result_loss.append(result[2])
+                        data = result_loss
+                    except:
+                        print('Error parse ping response')
                 """END TEST"""
                 if data:
-                    logging.info(u'Result from "{0}": "{1}"'.format(ip, data))
-                    self.metric_dealy_max.labels(ip, group).set(data[3])
-                    self.metric_dealy_min.labels(ip, group).set(data[2])
+                    print('Result from "{0}": "{1}"'.format(ip, data))
+                    #logging.info(u'Result from "{0}": "{1}"'.format(ip, data))
+                    self.metric_delay_max.labels(ip, group).set(data[3])
+                    self.metric_delay_min.labels(ip, group).set(data[2])
                     self.metric_delay.labels(ip, group).set(data[1])
                     self.metric_loss.labels(ip, group).set(data[0])
                     try:
                         push_to_gateway('graph.arhat.ua:9091', job='ping', registry=self.registry)
-                        logging.info(u'Push DONE')
+                        print('Push DONE')
+                        #logging.info(u'Push DONE')
                     except:
-                        logging.error(u'Error connect to push gate')
+                        print('Error connect to push gate')
+                        #logging.info(u'Error connect to push gate')
                 else:
                     self.metric_delay.labels(ip, group).set(0)
                     self.metric_loss.labels(ip, group).set(0)
-                    logging.error(u'some trable with ping')
+                    self.metric_delay_max.labels(ip, group).set(0)
+                    self.metric_delay_min.labels(ip, group).set(0)
+                    print('some trable with ping')
+                    #logging.info(u'some trable with ping')
             else:
                 self.metric_delay.labels(ip, group).set(0)
+                self.metric_loss.labels(ip, group).set(0)
+                self.metric_delay_max.labels(ip, group).set(0)
+                self.metric_delay_min.labels(ip, group).set(0)
                 task = asyncio.Task.current_task()
                 task.cancel()
-                logging.info(u'Cancel %s' % ip)
+                print('Cancel %s' % ip)
+                #logging.info(u'Cancel %s' % ip)
             yield from asyncio.sleep(int(self.delay_ping))
